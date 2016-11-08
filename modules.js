525024300e7e9d6a468788a33e1638eb00a4d451
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-11-08 08:49:56 -0700

    added modules file

---
+++ modules.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var fs = require('fs');
+var http = require('http');
+
+var express = require('express');
+var modal = require('react-modal');
+
+var goodbye = require('./goodbyes');
+var greeting = require('./greetings');
