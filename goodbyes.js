b9fd9b0ccb67f23ee64a1f0b6f6e01d09bdd23ec
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-11-08 08:30:34 -0700

    deleted solutions

--- goodbyes.js
+++ goodbyes.js
@@ -1,21 +1,6 @@
 'use strict';
 
 //export as a function (one method)
-module.exports = function(name){
-  console.log("Farewell, dear " + name);
-};
+
 
 //export as an object (three methods)
-module.exports = {
-  farewell: function(name){
-    console.log("Farewell, dear " + name);
-  }
-};
-
-module.exports.farewell = function(name){
-  console.log("Farewell, dear " + name);
-};
-
-exports.farewell = function(name){
-  console.log("Farewell, dear " + name);
-};
--- main.js
+++ main.js
@@ -1,29 +1,24 @@
 'use strict';
  var greeting =  require('./greetings');
- var goodbye = require('./goodbyes');
 
 greeting('Omar');
 greeting('Stringer');
 
+// ***** PART 1 ***** //
 
 //Explain to each other how the module.exports object works and how the require() function works.
 
 //If we execute the code in main.js, what can we expect to see in the terminal?
 
 
+// ***** PART 2 ***** //
 
+//Read "How do you split..." to remind yourself of what you saw in the video.
 
-
-// ***** STOP ***** //
-
-
-
-
-
-//Export the following function from goodbyes.js to main.js in four different ways (i.e., using four types of syntax), and then call it from main.js, printing to the console "Farewell, dear Kima".
+//Then, reference the examples shown there to export the following function from goodbyes.js to main.js in four different ways (i.e., using four types of syntax). Once exported, call the code from main.js, printing to the console "Farewell, dear Kima" (Make sure to check that each exported function works).
 
 // var farewell = function(name){
 //   console.log("Farewell, dear " + name);
 // };
-
-goodbye.farewell('Kima');
+//
+// farewell('Kima');
